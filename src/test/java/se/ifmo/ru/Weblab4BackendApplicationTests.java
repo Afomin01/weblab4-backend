b04e483d4f83518fc142893be19b012c7144281c
Afomin <284718@niuitmo.ru>
2020-10-27 15:17:58 +0300

    Initial commit

---
+++ src/test/java/se/ifmo/ru/Weblab4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ifmo.ru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Weblab4BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
