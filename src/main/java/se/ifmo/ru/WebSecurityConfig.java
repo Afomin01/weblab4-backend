fd9f2f585b282afa68e3edbd0f81b5bd84bfcc48
Dmitry Kupershtein <dmitry.kuper0@gmail.com>
2020-11-05 13:24:04 +0300

    allow rest login

--- src/main/java/se/ifmo/ru/WebSecurityConfig.java
+++ src/main/java/se/ifmo/ru/WebSecurityConfig.java
@@ -9,24 +9,31 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import se.ifmo.ru.services.UserService;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     @Autowired
     private UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-        .authorizeRequests()
-                .antMatchers("/signup").not().fullyAuthenticated()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/entries").hasRole("USER")
-        .and().httpBasic()
-        .and().sessionManagement().disable();
+        http.cors()
+                .and().csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/login*").permitAll()
+                    .antMatchers("/signup").not().fullyAuthenticated()
+                    .antMatchers("/admin").hasRole("ADMIN")
+                    .antMatchers("/entries").hasRole("USER")
+                .and().httpBasic();
     }
 
     @Bean
@@ -37,5 +44,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList(
+                "X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/se/ifmo/ru/controllers/LoginController.java
+++ src/main/java/se/ifmo/ru/controllers/LoginController.java
@@ -1,9 +1,11 @@
 package se.ifmo.ru.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/login")
 public class LoginController {
-
-
+    @GetMapping
+    public void login() {
+    }
 }
